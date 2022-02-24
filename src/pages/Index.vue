4a5473bd3bf6209f4d3e1cb23b213a5bbfc72d5b
ChintuSanty <hello@santy.in>
2022-02-24 01:16:44 +0000

    intial build

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="text-center container">
+      <q-img src="~assets/CILA-INCUBATOR-LOGO-CO.png" />
+      <div class="text-h4">UI Starter Kit</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  max-width: 1000px;
+  width: 90%;
+}
+</style>
